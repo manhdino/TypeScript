33d178215ca213a8c6cbea0d31bbcaa284e6d097
manhdino <manhnguyen1238@gmail.com>
2024-03-06 15:56:42 +0700

    Type Basics

--- sandbox.js
+++ sandbox.js
@@ -1,9 +1,32 @@
 var charactor = "Mario";
 console.log(charactor);
+var username = "Dinomanh";
+console.log(username);
 var inputs = document.querySelectorAll("input");
 console.log(inputs);
 // compile typescript to javascript
 // tsc sandbox.ts 
-inputs.forEach(function (input) {
-    console.log(input);
-});
+// tsc sandbox.ts -w : auto compile TS down to JS when save 
+// inputs.forEach(input => {
+//     console.log(input);
+// });
+//Type Basics 
+var char = "mario";
+var age = 30;
+var isAdmin = true;
+// char = 20;// Type 'number' is not assignable to type 'string'
+char = "Dinomanh 2"; //Correct
+//age = "Dinomanh" // Type 'string' is not assignable to type 'number'
+age = 45; //Correct
+//isAdmin = 'yes';//Type 'string' is not assignable to type 'boolean'.
+isAdmin = false;
+//function without define type for argument
+// const circle = (diameter)=>{
+//      return diameter * Math.PI;
+// }
+// console.log(circle('Hello world')); // Not Error -> return NaN: Not a Number
+var circle = function (diameter) {
+    return diameter * Math.PI;
+};
+console.log(circle(7.5)); // Show Error if pass string argument
+//If Error Occured in TS. It won't complied down to JS
--- sandbox.ts
+++ sandbox.ts
@@ -1,13 +1,42 @@
 const charactor = "Mario";
 console.log(charactor);
 
+const username = "Dinomanh";
+console.log(username);
 
 const inputs = document.querySelectorAll("input");
 console.log(inputs);
 
 // compile typescript to javascript
 // tsc sandbox.ts 
+// tsc sandbox.ts -w : auto compile TS down to JS when save 
 
-inputs.forEach(input => {
-    console.log(input);
-});
+// inputs.forEach(input => {
+//     console.log(input);
+// });
+
+//Type Basics 
+let char = "mario";
+let age = 30;
+let isAdmin = true;
+
+// char = 20;// Type 'number' is not assignable to type 'string'
+char = "Dinomanh 2";//Correct
+
+//age = "Dinomanh" // Type 'string' is not assignable to type 'number'
+age = 45;//Correct
+
+//isAdmin = 'yes';//Type 'string' is not assignable to type 'boolean'.
+isAdmin = false;
+
+//function without define type for argument
+// const circle = (diameter)=>{
+//      return diameter * Math.PI;
+// }
+// console.log(circle('Hello world')); // Not Error -> return NaN: Not a Number
+
+const circle = (diameter:number)=>{
+    return diameter * Math.PI;
+}
+console.log(circle(7.5)); // Show Error if pass string argument
+//If Error Occured in TS. It won't complied down to JS
