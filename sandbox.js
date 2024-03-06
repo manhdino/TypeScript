cfaa669845f26f900ebaaab05e5175068ef9f786
manhdino <manhnguyen1238@gmail.com>
2024-03-06 15:33:17 +0700

    compile TS down to JS

---
+++ sandbox.js
@@ -0,0 +1,9 @@
+var charactor = "Mario";
+console.log(charactor);
+var inputs = document.querySelectorAll("input");
+console.log(inputs);
+// compile typescript to javascript
+// tsc sandbox.ts 
+inputs.forEach(function (input) {
+    console.log(input);
+});
---
+++ sandbox.ts
@@ -0,0 +1,13 @@
+const charactor = "Mario";
+console.log(charactor);
+
+
+const inputs = document.querySelectorAll("input");
+console.log(inputs);
+
+// compile typescript to javascript
+// tsc sandbox.ts 
+
+inputs.forEach(input => {
+    console.log(input);
+});
